e598bcae8fc6cff40d67db76c790a70e7c1d8a36
Michał Banasiak <microbanasiak@gmail.com>
2024-12-21 19:32:46 +0100

    Upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//Program napisany przez Michała Banasiaka
+
+Console.WriteLine("Program autorstwa: Michała Banasiaka 2024");
+Console.WriteLine();
+Console.WriteLine("Witamy w grze na zgadywanie liczby! Podaj swoją liczbę:");
+Console.WriteLine("(Wylosowana liczba jest od 1 do 100).");
+
+int liczba = Convert.ToInt32(Console.ReadLine());
+int losowa = new Random().Next(1, 101);
+
+if (liczba == losowa)
+{
+    Console.WriteLine("Wygrałes!");
+}
+else
+{
+    Console.WriteLine("Przegrałes!");
+    if (liczba > losowa)
+    {
+        Console.WriteLine("Wylosowana liczba jest mniejsza od twojej o: " + (liczba - losowa));
+    }
+    else if (liczba < losowa)
+    {    
+        Console.WriteLine("Wylosowana liczba jest większa od twojej o: " + (losowa - liczba));
+    }
+
+}
